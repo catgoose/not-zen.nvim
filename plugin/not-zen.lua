3164d58ad6c39c055c781abaf192e252220239d7
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-01-29 19:54:36 -0600

    added user commands

--- plugin/not-zen.lua
+++ plugin/not-zen.lua
@@ -1,4 +1,5 @@
 local g = vim.g
+local api = vim.api
 
 if g.not_zen_loaded == 1 then
 	return
@@ -6,6 +7,18 @@ else
 	g.not_zen_loaded = 1
 end
 
-vim.api.nvim_create_user_command("NotZen", function()
+api.nvim_create_user_command("NotZen", function()
 	require("not-zen").toggle()
+end, {})
+
+api.nvim_create_user_command("NotZenToggle", function()
+	require("not-zen").toggle()
+end, {})
+
+api.nvim_create_user_command("NotZenOn", function()
+	require("not-zen").on()
+end, {})
+
+api.nvim_create_user_command("NotZenOff", function()
+	require("not-zen").off()
 end, {})
