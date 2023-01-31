d3c0bfed559a547556a8c5bbec02fdfb317faacf
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-01-30 19:32:11 -0600

    refactor state -> win_ids

--- lua/not-zen/window.lua
+++ lua/not-zen/window.lua
@@ -7,7 +7,7 @@ local ac = require("not-zen.autocmd")
 --  customize window highlights for pads
 
 local M = {}
-local state = {}
+local win_ids = {}
 
 local resize_pads = function()
 	local config = require("not-zen.config").init()
@@ -17,14 +17,15 @@ local resize_pads = function()
 	else
 		width = config.padding.width
 	end
-	api.nvim_win_set_width(state.left, width)
-	api.nvim_win_set_width(state.right, width)
+	api.nvim_win_set_width(win_ids.left, width)
+	api.nvim_win_set_width(win_ids.right, width)
 end
 
 local new_pads = function(new_cmd, direction)
 	cmd(new_cmd)
 	local win_id = api.nvim_get_current_win()
 	o.set_pad_options()
+	hl.padding_wins()
 	cmd("wincmd " .. direction)
 	return win_id
 end
@@ -33,16 +34,16 @@ M.create_layout = function()
 	if fn.filereadable(fn.expand("%:p")) ~= 1 or g.not_zen == true then
 		return
 	end
-	state.prev = api.nvim_get_current_win()
+	-- win_ids.prev = api.nvim_get_current_win()
 	local cur_pos = fn.getpos(".")
 	ac.quit_autocmd()
 	cmd.tabe("%")
 	o.set_options()
 	fn.setpos(".", cur_pos)
-	hl.main_win()
-	state.main = api.nvim_get_current_win()
-	state.left = new_pads("leftabove vnew", "l")
-	state.right = new_pads("vnew", "h")
+	hl.center_win()
+	win_ids.center = api.nvim_get_current_win()
+	win_ids.left = new_pads("leftabove vnew", "l")
+	win_ids.right = new_pads("vnew", "h")
 	resize_pads()
 end
 
